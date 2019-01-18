f6c0ef4ca7e8aefc27574e479e65e637869a3600
clementlauront <clement.lauront@gmail.com>
2019-01-18 09:17:43 +0100

    fix login post name

--- src/fr/agrondin/objects/LoginPostName.java
+++ src/fr/agrondin/objects/LoginPostName.java
@@ -3,15 +3,9 @@ package fr.agrondin.objects;
 public enum LoginPostName {
 	USERNAME("username"),
 	PASSWORD("password"),
-<<<<<<< HEAD
 	STAYCO("stayco"),
 	EMAIL("email");
-	
-=======
-	EMAIL("email"),
-	STAYCO("stayco");
 
->>>>>>> 0f06e41d2d4987a1582c492db5857d5976288582
 	private String name;
 	
 	public final String getName() {
